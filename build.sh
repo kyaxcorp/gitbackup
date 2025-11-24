2c4e74efcba4cc88296c83b45d97e112e8619317
Octavian T. <octavian@tabuci.org>
2025-11-24 22:31:28 +0200

    build v1.0.4

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 REPO="gitbackup/gitbackup"
-TAG_NAME="$REPO:v1.0.3"
+TAG_NAME="$REPO:v1.0.4"
 LATEST="$REPO:latest"
 
 docker buildx create --use --name mounted-build-kit --node mounted-build-kit --bootstrap
